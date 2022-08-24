20e61cf5603994e967de647993dce604b8c9314a
annaslipstick <63748586+annaslipstick@users.noreply.github.com>
2022-08-24 22:07:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+var json = [
+  {"name":"Name 1", "html_url": "http://www.example.com"},
+];
+
+function generateRandomLetter() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase()
+
+  return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText1").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText2").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText3").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText4").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText5").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText6").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText7").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText8").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText9").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText10").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText11").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText12").innerHTML +=  generateRandomLetter();
+}
+for(var i = 0; i < json.length; i++){
+	document.getElementById("pText13").innerHTML +=  generateRandomLetter();
+}
